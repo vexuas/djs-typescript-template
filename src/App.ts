a11092f03dd3cfc1535b83cc6a9e0a693e103f51
Gabriel R <42207245+vexuas@users.noreply.github.com>
2023-04-01 16:34:28 +0800

    Add main file (#6)
    * Create tsconfig
    
    * Create main file
    
    * Ignore environment file
    
    * Add scripts for building

---
+++ src/App.ts
@@ -0,0 +1,16 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import { BOT_TOKEN } from './config/environment';
+
+const app: Client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+const initialize = async (): Promise<void> => {
+  try {
+    await app.login(BOT_TOKEN);
+  } catch (error) {
+    throw error;
+  }
+};
+
+initialize();
