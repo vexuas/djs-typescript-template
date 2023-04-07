31ea8cc0384379b8171f31f61496fb22399ab234
Gabriel R <42207245+vexuas@users.noreply.github.com>
2023-04-07 18:10:47 +0800

    Add events structure (#9)
    * Create events file
    
    * Create ready event

---
+++ src/events/events.ts
@@ -0,0 +1,36 @@
+import { Client } from 'discord.js';
+import fs from 'fs';
+import path from 'path';
+
+interface Props {
+  app: Client;
+}
+type ExportedEventModule = {
+  default: (data: EventModule) => void;
+};
+export type EventModule = {
+  app: Client;
+};
+export function registerEventHandlers({ app }: Props): void {
+  const loadModules = (directoryPath: string) => {
+    fs.readdir(directoryPath, { withFileTypes: true }, (error, files) => {
+      if (error) {
+        //TODO: Replace with error handling
+        console.log(error);
+      }
+      files &&
+        files.forEach((file) => {
+          const filePath = path.join(directoryPath, file.name);
+          if (file.isDirectory()) {
+            return loadModules(filePath);
+          }
+          if (file.name === 'index.js') {
+            const modulePath = `.${filePath.replace('dist/events', '')}`;
+            const currentModule = require(modulePath) as ExportedEventModule;
+            currentModule.default({ app });
+          }
+        });
+    });
+  };
+  loadModules('./dist/events');
+}
