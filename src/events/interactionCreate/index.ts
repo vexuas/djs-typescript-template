40bfb28c3917c2b22ab89248886591fbb11b7926
Gab <rodriguezgab1601@gmail.com>
2023-04-08 16:00:33 +0800

    Forgot try catch

--- src/events/interactionCreate/index.ts
+++ src/events/interactionCreate/index.ts
@@ -4,13 +4,18 @@ import { EventModule } from '../events';
 
 export default function ({ app, appCommands }: EventModule) {
   app.on('interactionCreate', async (interaction: Interaction<CacheType>) => {
-    if (!interaction.inGuild() || !appCommands) return;
+    try {
+      if (!interaction.inGuild() || !appCommands) return;
 
-    if (interaction.isCommand()) {
-      const { commandName } = interaction;
-      const command = appCommands[commandName as keyof AppCommands];
-      command && (await command.execute({ interaction, app }));
+      if (interaction.isCommand()) {
+        const { commandName } = interaction;
+        const command = appCommands[commandName as keyof AppCommands];
+        command && (await command.execute({ interaction, app }));
+      }
+      //Maybe add buttons, selections and modal handlers here eventually
+    } catch (errors) {
+      //TODO: Add error handling
+      console.log(errors);
     }
-    //Maybe add buttons, selections and modal handlers here eventually
   });
 }
