26d3cabd370e73217a8ca2e642c698474c632aa4
Gabriel R <42207245+vexuas@users.noreply.github.com>
2023-04-09 18:48:16 +0800

    Add tests (#15)
    * Add config in package
    
    * Create embed for hello command
    
    * Add tests

---
+++ src/commands/hello/index.test.ts
@@ -0,0 +1,17 @@
+import { generateHelloEmbed } from '.';
+
+describe('Hello Command', () => {
+  it('generates an embed correctly', () => {
+    const embed = generateHelloEmbed();
+
+    expect(embed).not.toBeUndefined();
+  });
+  it('displays the correct fields in the embed', () => {
+    const embed = generateHelloEmbed();
+
+    expect(embed.description).not.toBeUndefined();
+    expect(embed.color).not.toBeUndefined();
+    expect(embed.fields).not.toBeUndefined();
+    expect(embed.fields && embed.fields.length).toBe(3);
+  });
+});
