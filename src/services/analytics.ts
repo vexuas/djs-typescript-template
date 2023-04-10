dd516b7d517e78ee7d4dab68ac07045d7207fcbf
Gab <rodriguezgab1601@gmail.com>
2023-04-11 01:16:44 +0800

    Add user profile

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,17 +1,46 @@
-import { Guild, TextBasedChannel, User } from 'discord.js';
+import {
+  DMChannel,
+  Guild,
+  PartialDMChannel,
+  PartialGroupDMChannel,
+  TextBasedChannel,
+  User,
+} from 'discord.js';
 import { capitalize } from 'lodash';
 import { Mixpanel } from 'mixpanel';
 
 type CommandEvent = {
+  client: Mixpanel;
   user: User;
   channel: TextBasedChannel;
   guild: Guild;
   command: string;
-  client: Mixpanel;
   options?: string;
   properties?: Object;
 };
+type UserProfile = {
+  client: Mixpanel;
+  user: User;
+  channel: Exclude<TextBasedChannel, DMChannel | PartialDMChannel | PartialGroupDMChannel>;
+  guild: Guild;
+  command?: string;
+};
 
+function setUserProfile({ client, user, channel, guild, command }: UserProfile) {
+  client.people.set(user.id, {
+    $name: user.username,
+    $created: user.createdAt.toISOString(),
+    tag: user.tag,
+    guild: guild.name,
+    guild_id: guild.id,
+  });
+  client.people.set_once(user.id, {
+    first_used: new Date().toISOString(), //Unfortunately this is only after v2.5
+    first_command: command,
+    first_used_in_guild: guild.name,
+    first_used_in_channel: channel.name,
+  });
+}
 export function sendCommandEvent({
   user,
   channel,
@@ -22,6 +51,7 @@ export function sendCommandEvent({
   properties,
 }: CommandEvent): void {
   if (channel.isDMBased()) return;
+  setUserProfile({ client, user, guild, channel, command });
   const eventName = `Use ${capitalize(command)} Command`;
 
   client.track(eventName, {
