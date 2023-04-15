d1440f6af031ce5a2825f0676698f6032fd24572
Gab <rodriguezgab1601@gmail.com>
2023-04-15 23:19:25 +0800

    Add test for help

---
+++ src/commands/help/index.test.ts
@@ -0,0 +1,17 @@
+import { generateHelpEmbed } from '.';
+
+describe('Help Command', () => {
+  it('generates an embed correctly', () => {
+    const embed = generateHelpEmbed();
+
+    expect(embed).not.toBeUndefined();
+  });
+  it('displays the correct fields in the embed', () => {
+    const embed = generateHelpEmbed();
+
+    expect(embed.description).not.toBeUndefined();
+    expect(embed.color).not.toBeUndefined();
+    expect(embed.fields).not.toBeUndefined();
+    expect(embed.fields && embed.fields.length).toBe(1);
+  });
+});
