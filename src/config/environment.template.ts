fe1aeda11e921bf4677913ccb440ad897c4a2fd0
Gab <rodriguezgab1601@gmail.com>
2023-06-06 03:19:10 +0800

    Create environment file template

---
+++ src/config/environment.template.ts
@@ -0,0 +1,21 @@
+//Bot Configuration; Required
+export const BOT_TOKEN = '';
+export const BOT_ID = '';
+export const GUILD_ID = '';
+
+//Environment
+export const ENV = 'dev';
+
+//Notifications
+export const GUILD_NOTIFICATION_WEBHOOK_URL = '';
+export const ERROR_NOTIFICATION_WEBHOOK_URL = '';
+export const BOOT_NOTIFICATION_CHANNEL_ID = '';
+
+//Database
+export const DATABASE_CONFIG = null;
+
+//Product Tracking
+export const MIXPANEL_ID = '';
+
+//TopGG Tracking
+export const TOP_GG_TOKEN = '';
