4fcf424dac971e0eba0ec47291125876d5617a6a
Gab <rodriguezgab1601@gmail.com>
2023-07-06 00:23:24 +0800

    Add autoposter on initialise

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,10 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-import { BOT_TOKEN, MIXPANEL_ID } from './config/environment';
+import { BOT_TOKEN, MIXPANEL_ID, TOP_GG_TOKEN } from './config/environment';
 import { registerEventHandlers } from './events/events';
 import { sendErrorLog } from './utils/helpers';
 import Mixpanel from 'mixpanel';
 import { isEmpty } from 'lodash';
+import AutoPoster from 'topgg-autoposter';
 
 const app: Client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -13,6 +14,7 @@ const initialize = async (): Promise<void> => {
   try {
     await app.login(BOT_TOKEN);
     const mixpanel = MIXPANEL_ID && !isEmpty(MIXPANEL_ID) ? Mixpanel.init(MIXPANEL_ID) : null;
+    TOP_GG_TOKEN && !isEmpty(TOP_GG_TOKEN) && AutoPoster(TOP_GG_TOKEN, app);
     registerEventHandlers({ app, mixpanel });
   } catch (error) {
     sendErrorLog({ error });
